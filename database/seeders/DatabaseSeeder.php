df8b8e15c6b35868a85154cb32f5da13caf72254
hossam elshahawy <hossamelshahawy13@gmail.com>
2025-09-09 11:50:44 +0300

    (add): seeder for driver

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            CompanySeeder::class
+            CompanySeeder::class,
+            DriverSeeder::class,
         ]);
     }
 }
--- database/seeders/DriverSeeder.php
+++ database/seeders/DriverSeeder.php
@@ -2,16 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Driver;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DriverSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        Driver::factory()->count(5)->create();
     }
 }
